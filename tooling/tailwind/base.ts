c19cfa2e2cb63fa4cb48ce4e83907a539b5ddc3e
fillip1984 <fillip1984@gmail.com>
2024-10-20 15:39:46 -0400

    updated colors

--- tooling/tailwind/base.ts
+++ tooling/tailwind/base.ts
@@ -5,7 +5,7 @@ export default {
   theme: {
     colors: {
       primary: "hsl(10, 74%, 60%)", //imperial red
-      secondary: "hsl(6, 77%, 62%)", // bittersweet
+      secondary: "hsl(210, 20%, 17%)", // bittersweet
       background: "hsl(0, 0%, 24%)", //onyx
       // primary: "#6E7E85",
       // secondary: "#E2E2E2",
@@ -15,7 +15,7 @@ export default {
       white: "hsl(55, 34%, 87%)", //eggshell
       gray: "hsl(210, 17%, 35%)", //Payne's gray
       danger: "hsl(358, 100%, 56%)", //Red (CMYK)
-      warning: "hsl(54, 63%, 71%", // Flax
+      warning: "hsl(54, 63%, 71%)", // Flax
       success: "hsl(133, 31%, 69%)", // Celadon
       info: "hsl(208, 52%, 83%)", // Columbia blue
     },
